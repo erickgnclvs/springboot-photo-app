b56b95cc8917d0b9837128a4515ec8d7e82ce375
Erick <erickgoncalves5@hotmail.com>
2023-03-15 17:37:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-photo-app
---
+++ src/test/java/edu/learning/photo/app/PhotoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.learning.photo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
