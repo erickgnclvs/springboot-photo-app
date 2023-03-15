f5283c1e004f92c3ab20ed68bd5d481de727a67a
Erick <erickgoncalves5@hotmail.com>
2023-03-15 17:53:51 -0300

    Work with GetMApping

--- src/main/java/edu/learning/photo/app/PhotoController.java
+++ src/main/java/edu/learning/photo/app/PhotoController.java
@@ -1,14 +1,22 @@
 package edu.learning.photo.app;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PhotoController {
 
-    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+    private Map<String, Photo> db =new HashMap<>() {{
+        put("1", new Photo("1", "hello.jpg"));
+    }};
 
     @GetMapping("/")
     public String hello() {
@@ -16,7 +24,18 @@ public class PhotoController {
     }
 
     @GetMapping("/photos")
-    public List<Photo> getPhotos() {
-        return db;
+    public Collection<Photo> getPhotos() {
+        return db.values();
     }
+
+    @GetMapping("/photos/{id}")
+    public Photo getPhoto(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return photo;
+    }
+
+
 }
